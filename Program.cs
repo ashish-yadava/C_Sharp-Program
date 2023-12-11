118c80a498d0fd34a67968a7102fbf9b837015cc
ASHISH YADAV <84899464+ashish-yadava@users.noreply.github.com>
2023-12-12 00:00:56 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PalindromeString
+namespace PalindromeStringUsingForLoop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            String str = "";
-            Console.WriteLine("Enter a string");
-            str = Console.ReadLine();
-
-            char[] arr = str.ToCharArray();
-            Array.Reverse(arr);
-            string str_reverse = new string(arr);
-
-            if (str.ToLower() == str_reverse.ToLower()) //(str.Equals(str_reverse)) 
+            String reverse = "";
+            Console.WriteLine("Enter A String");
+            string str = Console.ReadLine();
+            
+            for(int i = str.Length-1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            if(str.ToLower() == reverse.ToLower())
             {
                 Console.WriteLine(str + " --> It's A Palindrome String..");
             }
             else
             {
-                Console.WriteLine(str + " --> It's Not A Palindrome String");
+                Console.WriteLine(str + " --> It's Not A Palindrome String..");
             }
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }

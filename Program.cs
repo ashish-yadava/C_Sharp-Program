327723422dce81efa3f687547aae9d6081c028df
ASHISH YADAV <84899464+ashish-yadava@users.noreply.github.com>
2023-12-11 23:59:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "";
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string str_reverse = new string(arr);
+
+            if (str.ToLower() == str_reverse.ToLower()) //(str.Equals(str_reverse)) 
+            {
+                Console.WriteLine(str + " --> It's A Palindrome String..");
+            }
+            else
+            {
+                Console.WriteLine(str + " --> It's Not A Palindrome String");
+            }
+            Console.ReadLine();
+        }
+    }
+}
